0d721ea2051a5ca9cdfcc1515ea336f3d4da94bd
Carlos Rodriguez Dominguez <carlos@everywaretech.es>
2016-03-14 12:56:32 +0100

    Initial Commit

---
+++ GestionPiscinas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GestionPiscinas
+//
+//  Created by Carlos Rodríguez Domínguez on 14/3/16.
+//  Copyright © 2016 Carlos Rodríguez Domínguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GestionPiscinas/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GestionPiscinas
+//
+//  Created by Carlos Rodríguez Domínguez on 14/3/16.
+//  Copyright © 2016 Carlos Rodríguez Domínguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
