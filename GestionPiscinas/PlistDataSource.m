c121e9dc2e350265c1f6c1c95ee812d29b903ff4
Carlos Rodriguez Dominguez <carlos@everywaretech.es>
2016-05-06 20:19:44 +0200

    Terminada la implementación del plist data source.

--- GestionPiscinas/PlistDataSource.m
+++ GestionPiscinas/PlistDataSource.m
@@ -45,24 +45,71 @@ static PlistDataSource* dataSource = nil;
 {
     Piscina* piscina = [[Piscina alloc] initWithNombre:nombre];
     
+    //para agregar una piscina, vamos a obtener el listado completo de piscinas, agregarla la nueva, y guardar el listado completo de nuevo dentro del fichero
+    NSArray* piscinasLeidas = [self obtenerPiscinas];
+    NSMutableArray* piscinas = [[NSMutableArray alloc] initWithArray:piscinasLeidas];
+    [piscinas addObject:piscina];// agregamos la piscina al listado completo de piscinas en memoria
     
+    //guardamos el listado actualizado de piscinas en un fichero
+    [NSKeyedArchiver archiveRootObject:piscinas toFile:self.pathPlist];
     
     return piscina;
 }
 
 -(void)actualizarPiscina:(Piscina*)piscina
 {
+    //para actualizar una piscina, vamos a obtener el listado completo de piscinas, buscar el índice en el que estaba la antigua y volver a guardar el listado completo de nuevo dentro del fichero
+    NSArray* piscinasLeidas = [self obtenerPiscinas];
+    NSMutableArray* piscinas = [[NSMutableArray alloc] initWithArray:piscinasLeidas];
     
+    NSInteger indice = 0;
+    for (Piscina* p in piscinas){ //recorremos el listado de piscinas
+        if ([p.nombre isEqualToString:piscina.nombre]){ //si la piscina actual tiene el mismo nombre que la piscina que queremos actualizar, terminamos el bucle. indice contiene el índice en el que está la piscina que queremos actualizar
+            break;
+        }
+        indice++;
+    }
+    
+    if (indice < piscinas.count){ //hemos encontrado una piscina, puesto que si indice >= piscinas.count, entonces es que la piscina a actualizar no estaba en el array
+        [piscinas replaceObjectAtIndex:indice withObject:piscina];
+        
+        //guardamos el listado actualizado de piscinas en un fichero
+        [NSKeyedArchiver archiveRootObject:piscinas toFile:self.pathPlist];
+    }
 }
 
 -(void)eliminarPiscina:(Piscina*)piscina
 {
+    //para eliminar una piscina, vamos a obtener el listado completo de piscinas, buscar el índice en el que esta la piscina, eliminar la piscina del array y volver a guardar el listado completo de nuevo dentro del fichero
+    NSArray* piscinasLeidas = [self obtenerPiscinas];
+    NSMutableArray* piscinas = [[NSMutableArray alloc] initWithArray:piscinasLeidas];
     
+    NSInteger indice = 0;
+    for (Piscina* p in piscinas){ //recorremos el listado de piscinas
+        if ([p.nombre isEqualToString:piscina.nombre]){ //si la piscina actual tiene el mismo nombre que la piscina que queremos eliminar, terminamos el bucle. indice contiene el índice en el que está la piscina que queremos eliminar
+            break;
+        }
+        indice++;
+    }
+    
+    if (indice < piscinas.count){ //hemos encontrado una piscina, puesto que si indice >= piscinas.count, entonces es que la piscina a eliminar no estaba en el array
+        [piscinas removeObjectAtIndex:indice];
+        
+        //guardamos el listado actualizado de piscinas en un fichero
+        [NSKeyedArchiver archiveRootObject:piscinas toFile:self.pathPlist];
+    }
 }
 
 -(NSArray*)obtenerPiscinas
 {
-    
+    // obtiene el NSArray de piscinas desde el plist. NSKeyedUnarchiver es la API que permite leer desde un plist cualquier objeto guardado, teniendo que darle como parámetro el path del plist a leer.
+    NSArray* resultado = [NSKeyedUnarchiver unarchiveObjectWithFile:self.pathPlist];
+    if (resultado == nil){ //si el plist no existe, cuando nos pidan las piscinas, decimos que no hay (devolvemos un array vacio)
+        return [[NSArray alloc] init];
+    }
+    else{
+        return resultado; //devuelve las piscinas que estaban guardadas en el plist
+    }
 }
 
 @end
