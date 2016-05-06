6c827c40a2c19dabab780a1eec826239d141fb44
Carlos Rodriguez Dominguez <carlos@everywaretech.es>
2016-05-06 20:17:55 +0200

    Implementación más detallada del protocolo NSCoding.

--- GestionPiscinas/Medicion.m
+++ GestionPiscinas/Medicion.m
@@ -21,4 +21,22 @@
     return self;
 }
 
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    //este método viene del protocolo NSCoding y tenemos que implementarlo de tal manera que se lea del aDecoder que nos pasan como parámetros todos los properties que se hayan guardado previamente mediante el "encodeWithCoder:" (sentido: DataSource->Repository->ViewController)
+    self = [super init];
+    if (self) {
+        self.fecha = [coder decodeObjectForKey:@"fecha"];
+        self.parametros = [coder decodeObjectForKey:@"parametros"];
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder
+{
+    //este método viene del protocolo NSCoding y tenemos que implementarlo de tal manera que al objeto aCoder que nos dan como parámetro (el framework) le demos todos los properties de esta clase (sentido: ViewController->Repository->DataSource)
+    [coder encodeObject:self.fecha forKey:@"fecha"];
+    [coder encodeObject:self.parametros forKey:@"parametros"];
+}
+
 @end
