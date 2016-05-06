47d4cab513b0827b75d1838da694c09b052193f1
Carlos Rodriguez Dominguez <carlos@everywaretech.es>
2016-05-06 20:01:46 +0200

    Se ha creado un protocolo llamado GestorDatos para gestionar el guardado de la información en plist, memoria, cloud, etc.

---
+++ GestionPiscinas/GestorDatos.h
@@ -0,0 +1,22 @@
+//
+//  GestorDatos.h
+//  GestionPiscinas
+//
+//  Created by Carlos Rodríguez Domínguez on 14/3/16.
+//  Copyright © 2016 Carlos Rodríguez Domínguez. All rights reserved.
+//
+
+@import Foundation;
+#import "Piscina.h"
+#import "Medicion.h"
+#import "ParametrosMedicion.h"
+
+@protocol GestorDatos <NSObject>
+
++(Piscina*)agregarPiscina;
++(void)actualizarPiscina:(Piscina*)piscina;
++(void)eliminarPiscina:(Piscina*)piscina;
++(NSArray*)obtenerPiscinas;
+
+@end
+
