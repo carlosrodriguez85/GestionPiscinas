f87a22f075d7108a85d3ccf0b633d3891db34dc6
Carlos Rodriguez Dominguez <carlos@everywaretech.es>
2016-05-06 20:19:09 +0200

    Implementado el plist data source para que realmente guarde en ficheros.

--- GestionPiscinas/PlistDataSource.m
+++ GestionPiscinas/PlistDataSource.m
@@ -9,7 +9,7 @@
 #import "PlistDataSource.h"
 
 @interface PlistDataSource()
-
+@property (nonatomic, strong) NSString* pathPlist;
 @end
 
 @implementation PlistDataSource
@@ -20,7 +20,13 @@ static PlistDataSource* dataSource = nil;
 {
     self = [super init];
     if (self){
-        
+        //obtener el path para el fichero plist que va a guardar las piscinas. Ese fichero se va a llamar "piscinas.plist" y vamos a guardarlo en la carpeta "Documents".
+ //       NSSearchPathForDirectoriesInDomains(<#NSSearchPathDirectory directory#>, <#NSSearchPathDomainMask domainMask#>, <#BOOL expandTilde#>)
+        NSString* documentsPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0]; //hay que poner el [0] al final, porque este método devuelve un array con múltiples path para el path solicitado. Nosotros solo queremos el 0, porque desde hace bastantes años, en iOS es el único que importa. El índice 0, en este caso, corresponde al path del directory "Documents".
+        NSLog(@"Path carpeta Documents: %@", documentsPath);
+        NSString* pathPiscinasPlist = [documentsPath stringByAppendingPathComponent:@"piscinas.plist"]; //este método agrega al path el nombre del fichero que pasemos como parámetro, agregando las / u otros caracteres que sean necesarios.
+        self.pathPlist = pathPiscinasPlist;
+        NSLog(@"Path fichero piscinas.plist: %@", pathPiscinasPlist);
     }
     
     return self;
