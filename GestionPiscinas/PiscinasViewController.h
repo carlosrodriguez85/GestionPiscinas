00c0ecd35bb0bd5a7ff77ea8b1145bcdb7583ada
Carlos Rodriguez Dominguez <carlos@everywaretech.es>
2016-05-06 19:57:51 +0200

    Se ha agregado el PiscinasViewController.

---
+++ GestionPiscinas/PiscinasViewController.h
@@ -0,0 +1,13 @@
+//
+//  PiscinasViewController.h
+//  GestionPiscinas
+//
+//  Created by Carlos Rodríguez Domínguez on 14/3/16.
+//  Copyright © 2016 Carlos Rodríguez Domínguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PiscinasViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ GestionPiscinas/PiscinasViewController.m
@@ -0,0 +1,62 @@
+//
+//  PiscinasViewController.m
+//  GestionPiscinas
+//
+//  Created by Carlos Rodríguez Domínguez on 14/3/16.
+//  Copyright © 2016 Carlos Rodríguez Domínguez. All rights reserved.
+//
+
+#import "PiscinasViewController.h"
+
+@interface PiscinasViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation PiscinasViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableView datasource & delegate
+//El delegate y el datasource estan relacionados por storyboard, no por código
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"CeldaPiscina" forIndexPath:indexPath];
+    
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)pulsarAgregarPiscina:(id)sender {
+    
+}
+
+@end
